0cbdead674707f2802bf1051a9c607aa94a23e42
Rolimi9 <rolimi9@NeptuneU.local>
2017-05-16 23:53:17 +0800

    file downloads

--- mooncraft/mc01/urls.py
+++ mooncraft/mc01/urls.py
@@ -6,6 +6,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^enter/$', views.enter, name='enter'),
+    url(r'^enter/\d*/?$', views.enter, name='enter'),
+    url(r'^version/\d*/?$', views.version, name='version'),
+    url(r'^res/(?P<version>[.\w_-]+)/(?P<uri>.+)/$', views.res, name='res'),
     url(r'^res/(?P<uri>.+)/$', views.res, name='res'),
 ]
--- mooncraft/mc01/views.py
+++ mooncraft/mc01/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import GameUrl
+from django.http import StreamingHttpResponse
+from mooncraft.settings import STATIC_ROOT as static_root
 
 # Create your views here.
 from django.http import HttpResponse
@@ -14,5 +16,20 @@ def enter(request):
     url = GameUrl.objects.all()[0].res_url
     return HttpResponse(url)
 
-def res(request, uri):
-    return HttpResponse("res")
+def res(request, uri, version=None):
+    def file_iterator(file_name, chunk_size=512):
+        with open(file_name) as f:
+            while True:
+                c = f.read(chunk_size)
+                if c:
+                    yield c
+                else:
+                    break
+    file_name = static_root + uri
+    return StreamingHttpResponse(file_iterator(file_name))
+
+def version(request):
+    file_name = static_root + 'pack.version'
+    with open(file_name) as f:
+        c = f.read()
+    return HttpResponse(c)
